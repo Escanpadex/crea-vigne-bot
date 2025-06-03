8b376b4e2f8be297c6d752741c2a311db46ae7a9
Escanpadex <pierre.massif2@gmail.com>
2025-06-03 21:27:32 +0200

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,1 +1,34 @@
+// Configuration constants and variables
+const API_BASE = 'https://api.crea-vigne.fr/api';
 
+// Bot state variables
+let botRunning = false;
+let volumeScanInterval = null;
+let tradingLoopInterval = null;
+let statsInterval = null;
+let pnlUpdateInterval = null;
+let stopLossManagementInterval = null;
+let botStartTime = null;
+
+// Data storage
+let top30Pairs = [];
+let currentScanIndex = 0;
+let openPositions = [];
+let botStats = {
+    totalScans: 0,
+    totalSignals: 0,
+    totalPositions: 0
+};
+
+// Configuration object
+let config = {
+    apiKey: '',
+    secretKey: '',
+    passphrase: '',
+    capitalPercent: 5,
+    leverage: 2,
+    trailingStop: 1.0
+};
+
+// Balance information
+let balance = { USDT: 0, totalEquity: 0 }; 
